33fa6441ee91f024b6654c0e45fe67c9605919ce
zjn <zjn@abc.com>
2017-09-11 13:43:53 +0800

    bak

--- app/src/main/java/com/duowei/dw_pos/application/MyApplication.java
+++ app/src/main/java/com/duowei/dw_pos/application/MyApplication.java
@@ -1,9 +1,14 @@
 package com.duowei.dw_pos.application;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 
+import com.duowei.dw_pos.BuildConfig;
 import com.duowei.dw_pos.httputils.MyVolley;
 import com.duowei.dw_pos.httputils.NetUtils;
+import com.tencent.bugly.crashreport.CrashReport;
 
 import net.danlew.android.joda.JodaTimeAndroid;
 
@@ -20,5 +25,15 @@ public class MyApplication extends LitePalApplication {
         MyVolley.init(this);
         JodaTimeAndroid.init(this);
         NetUtils.init();
+
+        CrashReport.initCrashReport(getApplicationContext(), "11fd59b5eb", BuildConfig.DEBUG);
+        CrashReport.putUserData(getApplicationContext(), "MAC", getMacAddress());
+    }
+
+    @SuppressLint("HardwareIds")
+    private String getMacAddress() {
+        WifiManager manager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        WifiInfo info = manager.getConnectionInfo();
+        return info.getMacAddress();
     }
 }
